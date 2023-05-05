6370f588c28dd95216b36f58d14dbd231eea0212
Kevin Alavik <kevin@alavik.se>
2023-05-05 07:59:16 +0000

    Added canary

---
+++ projects/repojs/cdn/version@0.1/canary.js
@@ -0,0 +1,11 @@
+fetch('https://raw.githubusercontent.com/KevinAlavik/repojs/main/src/canary/index.js')
+  .then(response => response.text())
+  .then(data => {
+    const script = document.createElement('script');
+    script.textContent = data;
+    document.head.appendChild(script);
+  })
+  .catch(error => {
+    console.error('Error fetching script:', error);
+    alert("[REPOJS ERROR]\nCould'nt fetch repojs. Error: " + error)
+  });
