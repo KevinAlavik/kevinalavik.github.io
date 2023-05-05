94647d5eb7b0b70670cf202f876a9c2e5c710dc5
Kevin Alavik <95900603+KevinAlavik@users.noreply.github.com>
2023-05-05 16:14:20 +0200

    CDN Update

--- projects/repojs/cdn/version@0.1/canary.js
+++ projects/repojs/cdn/version@0.1/canary.js
@@ -1,11 +1,3 @@
-fetch('https://cdn.jsdelivr.net/gh/KevinAlavik/repojs/src/canary/index.js')
-  .then(response => response.text())
-  .then(data => {
-    const script = document.createElement('script');
-    script.textContent = data;
-    document.head.appendChild(script);
-  })
-  .catch(error => {
-    console.error('Error fetching script:', error);
-    alert("[REPOJS ERROR]\nCould'nt fetch repojs. Error: " + error)
-  });
+const script = document.createElement('script');
+script.src = "https://cdn.jsdelivr.net/gh/KevinAlavik/repojs/src/canary/index.js";
+document.head.appendChild(script);
